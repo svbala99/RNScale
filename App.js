449891b7f2e40daaa302c38d26169b78e4588258
Balamurugan <svbala99@gmail.com>
2022-05-29 18:27:03 +0530

    Chore: Height scale complete

--- App.js
+++ App.js
@@ -11,7 +11,21 @@ import Slider from '@react-native-community/slider';
  * @description the main App component
  */
 const App = () => {
+  // state variables
   const [weight, setWeight] = React.useState(10);
+  const [height, setHeight] = React.useState(40);
+
+  /**
+   * @function displayHeightText
+   * @param {Number} index
+   * @returns {String}
+   * @description Calculate height and pewrform conversion for ft in
+   */
+  const displayHeightText = index => {
+    const ft = Math.floor(index / 12);
+    const inch = index - 12 * ft;
+    return inch === 0 ? `${ft} ft` : '';
+  };
   return (
     <View style={styles.overallContainer}>
       {/* Weigh container */}
@@ -21,7 +35,6 @@ const App = () => {
           {`${weight} `}
           <Text style={styles.font24}>{'kg'}</Text>
         </Text>
-
         <ScrollView
           horizontal
           showsHorizontalScrollIndicator={true}
@@ -97,13 +110,16 @@ const App = () => {
           </View>
         </ScrollView>
       </View>
+
+      {/* Height container */}
       <View style={styles.weightContainer}>
         <Text style={styles.weighCaption}>{'How tall are you Mani?'}</Text>
-        <Text style={styles.weightDisplayText}>
-          {`${weight} `}
-          <Text style={styles.font24}>{'kg'}</Text>
+        <Text style={styles.heightDisplayText}>
+          {`${Math.floor(height / 12)} `}
+          <Text style={styles.font18}>{'ft'}</Text>
+          {` ${height - 12 * Math.floor(height / 12)} `}
+          <Text style={styles.font18}>{'in'}</Text>
         </Text>
-        {/* Height container */}
         <ScrollView
           horizontal
           showsHorizontalScrollIndicator={true}
@@ -112,20 +128,23 @@ const App = () => {
           <Slider
             thumbImage={require('./assets/img/marker.png')}
             tapToSeek={true}
-            onValueChange={v => setWeight(v)}
+            onValueChange={v => setHeight(v)}
             minimumValue={0}
             maximumValue={100}
             minimumTrackTintColor={'white'}
             maximumTrackTintColor={'white'}
             step={1}
-            value={weight}
+            value={height}
           />
           <View style={styles.markingContainer}>
             {Array(101)
               .fill()
               .map((_, i) => i)
               .map((value, index) => {
-                if ((index > 0 && index % 5 === 0) || index === 0) {
+                if (
+                  (index > 0 && index % 2 === 0 && index % 3 === 0) ||
+                  index === 0
+                ) {
                   return (
                     <Text
                       key={index.toString()}
@@ -133,7 +152,7 @@ const App = () => {
                       {'|'}
                     </Text>
                   );
-                } else if (index > 0 && index % 5 !== 0) {
+                } else if (index > 0 && (index % 2 !== 0 || index % 3 !== 0)) {
                   return (
                     <Text
                       key={index.toString()}
@@ -149,29 +168,32 @@ const App = () => {
               .fill()
               .map((_, i) => i)
               .map((value, index) => {
-                if ((index > 0 && index % 5 === 0) || index === 0) {
+                if (
+                  (index > 0 && index % 3 === 0 && index % 2 === 0) ||
+                  index === 0
+                ) {
                   return (
                     <Text
                       key={index.toString()}
                       style={[
-                        styles.weighGraduationText,
+                        styles.heightGraduationText,
                         // eslint-disable-next-line react-native/no-inline-styles
                         {
                           paddingRight:
-                            index === 0
-                              ? 30
-                              : index <= 10
-                              ? 28
+                            index <= 10
+                              ? 40
                               : index <= 40
-                              ? 22
+                              ? 44
                               : index <= 60
-                              ? 20
+                              ? 42
                               : index <= 70
-                              ? 22
-                              : 20,
+                              ? 42
+                              : index <= 90
+                              ? 42
+                              : 42,
                         },
                       ]}>
-                      {index}
+                      {displayHeightText(index)}
                     </Text>
                   );
                 }
@@ -194,10 +216,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     overflow: 'scroll',
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    marginBottom: 32,
   },
   weighCaption: {fontWeight: 'bold', fontSize: 20, paddingBottom: 24},
   weightDisplayText: {fontWeight: 'bold', fontSize: 32},
+  heightDisplayText: {fontWeight: 'bold', fontSize: 32, paddingBottom: 24},
   font24: {fontSize: 24},
+  font18: {fontSize: 18},
   horizontalScrollContainer: {
     width: 970,
     height: 100,
@@ -227,6 +254,7 @@ const styles = StyleSheet.create({
     paddingTop: 24,
   },
   weighGraduationText: {color: 'grey', fontSize: 20},
+  heightGraduationText: {color: 'grey', fontSize: 14},
 });
 
 export default App;
